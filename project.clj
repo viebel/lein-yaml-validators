de191d7d7e4b2d825ecdb7218f9ea8cc9612aea7
ashercoren <ashercoren@gmail.com>
2015-03-02 13:30:59 +0200

    move from s3

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject audyx-s3/lein-yaml-validators "0.0.1"
+(defproject lein-yaml-validators "0.0.1"
   :description "YAML validaton functions for clojure projects"
   :url "https://github.com/Audyx/lein-yaml-validators"
-  :repositories [["audyx-s3" {:url "s3p://clojurescript/releases" :creds :gpg}]]
-  :license {:name "Audyx License"
-            :url "http://www.audyx.com/mentions-legales/"}
-  :plugins [[s3-wagon-private "1.1.2"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-yaml "0.4.0"]
                  [viebel/audyx-toolbet "0.0.17"]
-                 [me.raynes/fs "1.4.6"]
-                 [io.aviso/pretty "0.1.17"]]
+                 [me.raynes/fs "1.4.6"]]
   :eval-in-leiningen true)
