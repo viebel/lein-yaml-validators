e4ac66f969d3c055f71ca1ba43f704a307d24a5e
ashercoren <ashercoren@gmail.com>
2015-03-02 12:55:02 +0200

    hide credentials

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject audyx-s3/lein-yaml-validators "0.0.1"
   :description "YAML validaton functions for clojure projects"
   :url "https://github.com/Audyx/lein-yaml-validators"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["audyx-s3" {:url "s3p://clojurescript/releases" :creds :gpg}]]
+  :license {:name "Audyx License"
+            :url "http://www.audyx.com/mentions-legales/"}
+  :plugins [[s3-wagon-private "1.1.2"]]
   :dependencies [[clj-yaml "0.4.0"]
                  [viebel/audyx-toolbet "0.0.17"]
                  [me.raynes/fs "1.4.6"]
